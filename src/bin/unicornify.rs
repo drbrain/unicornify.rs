7f05ecf6ab27c7abbd1cf09ef95393d131a1e68e
Eric Hodel <drbrain@segment7.net>
2020-11-17 18:03:32 -0800

    Allow generating unicorns by hash

--- src/bin/unicornify.rs
+++ src/bin/unicornify.rs
@@ -1,9 +1,17 @@
-use unicornify::Avatar;
-
 use image::DynamicImage;
 
+use std::env::args;
+
+use unicornify::Avatar;
+
 fn main() {
-    let hash = String::from("58479f76374a3ba3c69b9804163f39f4");
+    let hash = match args().nth(1) {
+        Some(h) => h,
+        None => String::from("58479f76374a3ba3c69b9804163f39f4"),
+    };
+
+    dbg!(hash.clone());
+
     let avatar = Avatar::new(hash, false).unwrap();
     let image_buffer = avatar.draw(128, true, false, false, false, false);
 
