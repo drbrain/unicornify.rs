b308fbc902394bc7390971934b492b396ef6e77d
Eric Hodel <drbrain@segment7.net>
2020-11-19 00:13:31 -0800

    Parameterize quadrant for debugging

--- src/bin/unicornify.rs
+++ src/bin/unicornify.rs
@@ -9,9 +9,13 @@ fn main() {
         Some(h) => h,
         None => String::from("58479f76374a3ba3c69b9804163f39f4"),
     };
+    let quadrant = match args().nth(2) {
+        Some(q) => Some(q.parse::<u8>().unwrap()),
+        None => None,
+    };
 
     let avatar = Avatar::new(hash, false).unwrap();
-    let image_buffer = avatar.draw(128, true, false, false, false, false);
+    let image_buffer = avatar.draw(128, quadrant, true, false, false, false, false);
 
     let image = DynamicImage::ImageRgba8(image_buffer);
     image.save("out.png").unwrap();
